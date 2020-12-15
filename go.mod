01ad073ab8461efd96142cb23eda742a820bbae1
Henri Lepic <henrilepic@gmail.com>
2020-12-15 14:51:39 +0100

    add swagger and patch service

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/dgkg/keypass
 go 1.15
 
 require (
-	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
+	github.com/gin-gonic/gin v1.6.3
 	github.com/gofrs/uuid v3.3.0+incompatible
+	github.com/swaggo/files v0.0.0-20190704085106-630677cd5c14
+	github.com/swaggo/gin-swagger v1.3.0
+	github.com/swaggo/swag v1.5.1
 )
--- main.go
+++ main.go
@@ -1,26 +1,49 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gofrs/uuid"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 
+	_ "github.com/dgkg/keypass/docs"
 	"github.com/dgkg/keypass/model"
 )
 
+// @title Swagger For Keypass API
+// @version 1.0
+// @description This is an API for creating hash in order to create keypasses.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host petstore.swagger.io
+// @BasePath /v2
 func main() {
 
-	UsersDB = make(map[string]*model.User)
+	DB.users = make(map[string]*model.User)
 
-	router := gin.Default()
+	r := gin.Default()
 
-	router.GET("/users/:uuid", GetUser)
-	router.GET("/users/:uuid/*action", SetUserAction)
-	router.POST("/users", CreateUser)
+	url := ginSwagger.URL("http://localhost:9090/swagger/doc.json") // The url pointing to API definition
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
+	r.GET("/users/:uuid", GetUser)
+	r.GET("/users/:uuid/*action", SetUserAction)
+	r.PATCH("/users/:uuid", UpdateUser)
+	r.PUT("/users/:uuid", UpdateUser)
 
-	router.Run(":9090")
+	r.POST("/users", CreateUser)
+
+	r.Run(":9090")
 }
 
 func SetUserAction(c *gin.Context) {
@@ -30,6 +53,14 @@ func SetUserAction(c *gin.Context) {
 	c.String(http.StatusOK, message)
 }
 
+// @Description get a User by ID
+// @Accept json
+// @Produce json
+// @Param uuid path string true "Some ID"
+// @Success 200 {string} string "ok"
+// @Failure 400 {string} string "We need ID!!"
+// @Failure 404 {string} string "Can not find ID"
+// @Router /users/{some_id} [get]
 func GetUser(ctx *gin.Context) {
 
 	id, err := uuid.FromString(ctx.Param("uuid"))
@@ -39,7 +70,7 @@ func GetUser(ctx *gin.Context) {
 		return
 	}
 
-	u, ok := UsersDB[id.String()]
+	u, ok := DB.users[id.String()]
 	if !ok {
 		ctx.JSON(http.StatusNotFound, nil)
 	}
@@ -60,9 +91,66 @@ func CreateUser(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, nil)
 		return
 	}
-	UsersDB[u2.ID] = u2
+	DB.users[u2.ID] = u2
 	ctx.JSON(http.StatusOK, u2)
 }
 
-// UsersDB is a moke for DB.
-var UsersDB map[string]*model.User
+func UpdateUser(ctx *gin.Context) {
+	id, err := uuid.FromString(ctx.Param("uuid"))
+	if err != nil {
+		log.Println("/users bad request", err.Error())
+		ctx.JSON(http.StatusBadRequest, nil)
+		return
+	}
+	var payload Payloadpatch
+	payload.data = make(map[string]interface{})
+	err = ctx.BindJSON(&payload.data)
+	if err != nil {
+		log.Println("/users bad request", err.Error())
+		ctx.JSON(http.StatusBadRequest, nil)
+		return
+	}
+
+	u, ok := DB.users[id.String()]
+	if !ok {
+		ctx.JSON(http.StatusNotFound, nil)
+		return
+	}
+
+	u.FirstName = payload.toString("first_name")
+	u.LastName = payload.toString("last_name")
+	u.Email = payload.toString("email")
+
+	if len(payload.errs) != 0 {
+		ctx.JSON(http.StatusBadRequest, nil)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, u)
+}
+
+type Payloadpatch struct {
+	data map[string]interface{}
+	errs []error
+}
+
+func (p *Payloadpatch) toString(fieldName string) string {
+	val, ok := p.data[fieldName]
+	if !ok {
+		p.errs = append(p.errs, errors.New("no values for:"+fieldName))
+		return ""
+	}
+	newval, ok := val.(string)
+	if !ok {
+		p.errs = append(p.errs, errors.New("cast not possible into string for:"+fieldName))
+		return ""
+	}
+	return newval
+}
+
+type mokeDB struct {
+	users map[string]*model.User
+}
+
+// DB is a moke for DB.
+var DB mokeDB
